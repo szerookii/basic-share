e5a20c414d658f9e5060d66db9462c1ef9ee892d
git <36236750+szerookii@users.noreply.github.com>
2025-04-06 15:35:36 +0200

    typo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "fr.szeroki.basicshare"
-    compileSdk = flutter.compileSdkVersion`
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -24,7 +24,6 @@ android {
         applicationId = "fr.szeroki.basicshare"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 19
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
